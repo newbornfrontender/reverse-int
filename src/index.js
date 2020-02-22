7b777836c5ab9220495563f9acef94fc5c869e39
newbornfrontender <newbornfrontender@gmail.com>
2020-02-22 23:12:50 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  const getEither = (n) => Math.sign(n);
+  const toPositive = (n) => n * -1;
+  const isPositive = getEither(n) === -1;
+
+  n = isPositive ? toPositive(n) : n;
+
+  return n.toString().split('').reverse().join('');
 }
